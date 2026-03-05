8dfe90c924fed12c0907b009e90b9d207b603e65
Ricky Park <heritage@rickypark.com>
2026-03-05 17:30:52 +0800

    shimmer

--- tailwind.config.js
+++ tailwind.config.js
@@ -90,6 +90,9 @@ module.exports = {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
       },
+      shimmer: {
+      '100%': { transform: 'translateX(100%)' },
+      }
     },
   },
   plugins: [require("tailwindcss-animate")],
